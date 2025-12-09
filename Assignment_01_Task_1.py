875f1d8bbcdc4ed11815d24438b631c5435024f4
prasadslonkar <prasadslonkar@gmail.com>
2025-12-09 22:17:18 +0530

    Add files via upload

---
+++ Assignment_01_Task_1.py
@@ -0,0 +1,7 @@
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number(non-zero): "))
+
+print(f"Addition :{a+b}")
+print(f"Subtraction :{a-b}")
+print(f"Multiplication:{a*b}")
+print(f"Division:{a/b}")
